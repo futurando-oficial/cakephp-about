b3464ebf1561526ca3bd1bc8f98e60171bffe0eb
Gustavo <guto00741@hotmail.com>
2018-09-04 21:23:10 -0300

    api/routes

--- config/routes.php
+++ config/routes.php
@@ -4,9 +4,16 @@ use Cake\Routing\Router;
 use Cake\Routing\Route\DashedRoute;
 
 Router::plugin(
-    'About',
+    'about',
     ['path' => '/about'],
     function (RouteBuilder $routes) {
         $routes->fallbacks(DashedRoute::class);
     }
 );
+
+Router::prefix('painel', ['path' => '/painel'], function ($routes) {
+    $routes->plugin('About', function ($routes) {
+        $routes->connect('/', ['controller' => 'About', 'action' => 'index']);
+        $routes->fallbacks('DashedRoute');
+    });
+});
